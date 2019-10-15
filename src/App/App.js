9693b6aa3092c96ca8883b43a0ea92da964884f6
mari4857 <mar4857@gmail.com>
2019-10-15 16:14:45 -0400

    adding dummyStore data and starter content to App.js

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { Component }from 'react';
+import { Route } from 'react-router-dom';
+import dummyStore from '../dummy-store';
 
-function App() {
-  return (
-      <main className='App'>
-        {/* content goes here */}
-      </main>
-  );
+class App extends Component {
+  state = {
+    folders: [],
+    notes: []
+  };
+
+  render() {
+    return (
+        <div className='app'>
+          <nav className='app-nav'>
+            <h1>Noteful app nav</h1>
+          </nav>
+          <header className='app-header'>
+            <p>App header</p>
+          </header>
+          <main className='app-main'>
+            <p>App main</p>
+          </main>
+        </div>
+    );
+  }
 }
 
 export default App;
