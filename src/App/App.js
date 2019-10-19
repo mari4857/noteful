d34e7936e773e8d86289cbd0d535ac9f796e47b7
mariana-gibaldi <mariana.gibaldi@coresecure.com>
2019-10-19 12:06:20 -0400

    adding nav routes

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,8 @@
 import React, { Component }from 'react';
 import { Route } from 'react-router-dom';
+import NoteListNav from '../NoteListNav/NoteListNav';
+import NotePageNav from '../NotePageNav/NotePageNav';
+import {getNotesForFolder, findNote, findFolder} from '../notes-helpers';
 import dummyStore from '../dummy-store';
 
 class App extends Component {
@@ -16,19 +19,42 @@ class App extends Component {
   renderNavRoutes() {
     const {folders, notes} = this.state;
     return (
-        <>
-        </>
-    )
+      <>
+        {['/', '/folder/:folderId'].map(path => (
+          <Route
+            exact
+            key={path}
+            path={path}
+            render={routeProps => (
+              <NoteListNav
+                folders={folders}
+                notes={notes}
+                {...routeProps}
+              />
+            )}
+          />
+        ))}
+        <Route
+          path="/note/:noteId"
+          render={routeProps => {
+            const {noteId} = routeProps.match.params;
+            const note = findNote(notes, noteId) || {};
+            const folder = findFolder(folders, note.folderId);
+            return <NotePageNav {...routeProps} folder={folder} />;
+          }}
+        />
+        <Route path="/add-folder" component={NotePageNav} />
+        <Route path="/add-note" component={NotePageNav} />
+      </>
+    );
   }
 
   render() {
     return (
         <div className='app'>
-          <nav className='app-nav'>
-            <h1>Noteful app nav</h1>
-          </nav>
+          <nav className='app-nav'>{this.renderNavRoutes()}</nav>
           <header className='app-header'>
-            <p>App header</p>
+            <h1>Noteful app header</h1>
           </header>
           <main className='app-main'>
             <p>App main</p>
