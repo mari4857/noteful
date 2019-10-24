4454208f7a2b54ababd9f8df84e8484b274537b9
mari4857 <mar4857@gmail.com>
2019-10-24 17:55:38 -0400

    adding NotePageMain, NoteListMain and Note components

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,13 @@
 import React, { Component }from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import NoteListNav from '../NoteListNav/NoteListNav';
 import NotePageNav from '../NotePageNav/NotePageNav';
-import {getNotesForFolder, findNote, findFolder} from '../notes-helpers';
+import NoteListMain from '../NoteListMain/NoteListMain';
+import NotePageMain from '../NotePageMain/NotePageMain';
 import dummyStore from '../dummy-store';
+import {getNotesForFolder, findNote, findFolder} from '../notes-helpers';
+import './App.css';
 
 class App extends Component {
   state = {
@@ -46,6 +50,42 @@ class App extends Component {
         <Route path="/add-folder" component={NotePageNav} />
         <Route path="/add-note" component={NotePageNav} />
       </>
+    );
+  }
+
+  renderMainRoutes() {
+    const {notes, folders} = this.state;
+    return (
+        <>
+          {['/', '/folder/:folderId'].map(path => (
+              <Route
+                  exact
+                  key={path}
+                  path={path}
+                  render={routeProps => {
+                    const {folderId} = routeProps.match.params;
+                    const notesForFolder = getNotesForFolder(
+                        notes,
+                        folderId
+                    );
+                    return (
+                        <NoteListMain
+                            {...routeProps}
+                            notes={notesForFolder}
+                        />
+                    );
+                  }}
+              />
+          ))}
+          <Route
+              path="/note/:noteId"
+              render={routeProps => {
+                const {noteId} = routeProps.match.params;
+                const note = findNote(notes, noteId);
+                return <NotePageMain {...routeProps} note={note} />;
+              }}
+          />
+        </>
     );
   }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  faPlus, faChevronLeft, faTrashAlt, faCheckDouble
+} from '@fortawesome/free-solid-svg-icons'
 import { BrowserRouter } from 'react-router-dom'
+import 'typeface-roboto'
 import App from './App/App';
 import './index.css';
+
+library.add(faPlus, faChevronLeft, faTrashAlt, faCheckDouble)
 
 ReactDOM.render(
   <BrowserRouter>
