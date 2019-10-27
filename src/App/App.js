e20467620e8b849a54da630eaf016f46bd8fd1a3
mariana-gibaldi <mariana.gibaldi@coresecure.com>
2019-10-27 17:25:35 -0400

    adding initial css and fixing routes

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,5 @@
-import React, { Component }from 'react';
-import { Route, Link } from 'react-router-dom';
+import React, {Component} from 'react';
+import {Route, Link} from 'react-router-dom';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import NoteListNav from '../NoteListNav/NoteListNav';
 import NotePageNav from '../NotePageNav/NotePageNav';
@@ -11,8 +11,8 @@ import './App.css';
 
 class App extends Component {
   state = {
-    folders: [],
-    notes: []
+    notes: [],
+    folders: []
   };
 
   componentDidMount() {
@@ -21,7 +21,7 @@ class App extends Component {
   }
 
   renderNavRoutes() {
-    const {folders, notes} = this.state;
+    const {notes, folders} = this.state;
     return (
       <>
         {['/', '/folder/:folderId'].map(path => (
@@ -56,50 +56,51 @@ class App extends Component {
   renderMainRoutes() {
     const {notes, folders} = this.state;
     return (
-        <>
-          {['/', '/folder/:folderId'].map(path => (
-              <Route
-                  exact
-                  key={path}
-                  path={path}
-                  render={routeProps => {
-                    const {folderId} = routeProps.match.params;
-                    const notesForFolder = getNotesForFolder(
-                        notes,
-                        folderId
-                    );
-                    return (
-                        <NoteListMain
-                            {...routeProps}
-                            notes={notesForFolder}
-                        />
-                    );
-                  }}
-              />
-          ))}
+      <>
+        {['/', '/folder/:folderId'].map(path => (
           <Route
-              path="/note/:noteId"
-              render={routeProps => {
-                const {noteId} = routeProps.match.params;
-                const note = findNote(notes, noteId);
-                return <NotePageMain {...routeProps} note={note} />;
-              }}
+            exact
+            key={path}
+            path={path}
+            render={routeProps => {
+              const {folderId} = routeProps.match.params;
+              const notesForFolder = getNotesForFolder(
+                notes,
+                folderId
+              );
+              return (
+                <NoteListMain
+                  {...routeProps}
+                  notes={notesForFolder}
+                />
+              );
+            }}
           />
-        </>
+        ))}
+        <Route
+          path="/note/:noteId"
+          render={routeProps => {
+            const {noteId} = routeProps.match.params;
+            const note = findNote(notes, noteId);
+            return <NotePageMain {...routeProps} note={note} />;
+          }}
+        />
+      </>
     );
   }
 
   render() {
     return (
-        <div className='app'>
-          <nav className='app-nav'>{this.renderNavRoutes()}</nav>
-          <header className='app-header'>
-            <h1>Noteful app header</h1>
-          </header>
-          <main className='app-main'>
-            <p>App main</p>
-          </main>
-        </div>
+      <div className="App">
+        <nav className="App__nav">{this.renderNavRoutes()}</nav>
+        <header className="App__header">
+          <h1>
+            <Link to="/">Noteful</Link>{' '}
+            <FontAwesomeIcon icon="check-double" />
+          </h1>
+        </header>
+        <main className="App__main">{this.renderMainRoutes()}</main>
+      </div>
     );
   }
 }
--- src/Note/Note.js
+++ src/Note/Note.js
@@ -6,26 +6,26 @@ import './Note.css'
 
 export default function Note(props) {
   return (
-      <div className='Note'>
-        <h2 className='Note__title'>
-          <Link to={`/note/${props.id}`}>
-            {props.name}
-          </Link>
-        </h2>
-        <button className='Note__delete' type='button'>
-          <FontAwesomeIcon icon='trash-alt' />
+    <div className='Note'>
+      <h2 className='Note__title'>
+        <Link to={`/note/${props.id}`}>
+          {props.name}
+        </Link>
+      </h2>
+      <button className='Note__delete' type='button'>
+        <FontAwesomeIcon icon='trash-alt' />
+        {' '}
+        remove
+      </button>
+      <div className='Note__dates'>
+        <div className='Note__dates-modified'>
+          Modified
           {' '}
-          remove
-        </button>
-        <div className='Note__dates'>
-          <div className='Note__dates-modified'>
-            Modified
-            {' '}
-            <span className='Date'>
+          <span className='Date'>
             {format(props.modified, 'Do MMM YYYY')}
           </span>
-          </div>
         </div>
       </div>
+    </div>
   )
 }
